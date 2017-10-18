98e57ada92709ceb9530270cf71f84ec9ab9ec1e
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-10-19 01:56:49 +0330

    Login page has been made

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -3,7 +3,10 @@ import React, {Component} from 'react';
 import {
     View,
     Text,
-    StyleSheet
+    TextInput,
+    Image,
+    StyleSheet,
+    TouchableOpacity
 } from 'react-native';
 
 class MainScreen extends Component {
@@ -12,11 +15,24 @@ class MainScreen extends Component {
         header: null
     };
 
+    login () {
+        const username = this.refs.username._lastNativeText;
+        const password = this.refs.password._lastNativeText;
+        // do something with them
+    }
+
     render() {
         const {navigate} = this.props.navigation;
         return (
             <View style={styles.container}>
-                <Text style={styles.text}>سلام بر شما</Text>
+                <Image source={require('../assets/images/logo.png')} style={styles.logo} />
+                <TextInput ref="username" placeholder="نام کاربری" style={styles.input} />
+                <TextInput ref="password" placeholder="گذرواژه" style={styles.input} secureTextEntry={true} />
+                <TouchableOpacity activeOpacity={0.6} style={styles.submitContainer} onPress={this.login.bind(this)}>
+                    <View style={styles.submit}>
+                        <Text style={styles.submitText}>ورود</Text>
+                    </View>
+                </TouchableOpacity>
             </View>
         );
     }
@@ -24,13 +40,41 @@ class MainScreen extends Component {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
     },
-    text: {
-        fontSize: 25,
-        fontFamily: 'iransans'
+    logo: {
+        width: 180,
+        height: 140,
+        marginBottom: 15
+    },
+    input: {
+        marginTop: 5,
+        fontSize: 17,
+        fontFamily: 'iransans',
+        fontWeight: 'normal',
+        width: 250,
+        textAlign: 'center'
+    },
+    submitContainer: {
+        marginTop: 15,
+    },
+    submit: {
+        width: 250,
+        height: 40,
+        backgroundColor: '#ed5900',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    submitText: {
+        fontSize: 17,
+        fontFamily: 'iransans',
+        fontWeight: 'normal',
+        textAlign: 'center',
+        color: 'white'
     }
+
 });
 
 export default MainScreen;
