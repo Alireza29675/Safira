99a65bfbacfff30bdb6bf5086f0065e2400b4a92
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-10-19 02:45:20 +0330

    Header is done

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -4,7 +4,8 @@ import {
     View,
     Text,
     Image,
-    StyleSheet
+    StyleSheet,
+    TouchableOpacity
 } from 'react-native';
 
 class MainScreen extends Component {
@@ -13,12 +14,22 @@ class MainScreen extends Component {
         header: null
     };
 
+    goToProfile () {
+        console.log('Profile')
+    }
+
     render() {
-        const {navigate} = this.props.navigation;
+        const { navigate } = this.props.navigation;
         return (
             <View>
                 <View style={styles.header}>
                     <Image style={styles.headerImage} source={require('../assets/images/tehran.jpg')} />
+                    <TouchableOpacity activeOpacity={0.8} style={styles.userImageContainer}>
+                        <Image style={styles.userImage} source={require('../assets/images/user/avatar.jpg')} onPress={this.goToProfile.bind(this)} />
+                    </TouchableOpacity>
+                </View>
+                <View>
+
                 </View>
             </View>
         );
@@ -28,11 +39,24 @@ class MainScreen extends Component {
 const styles = StyleSheet.create({
     header: {
         width: '100%',
-        height: 140
+        height: 170
     },
     headerImage: {
         width: '100%',
-        height: 140
+        height: 170
+    },
+    userImageContainer: {
+        position: 'absolute',
+        left: '50%',
+        bottom: -40,
+        transform: [{ translateX: -70 }]
+    },
+    userImage: {
+        width: 140,
+        height: 140,
+        borderRadius: 10,
+        borderColor: 'white',
+        borderWidth: 2,
     }
 });
 
